8cfa96016d279bbd4d7be57f9304944909311b8c
Sarah Young <sarah.young@system1group.com>
2017-08-25 11:34:15 +0100

    Test Automation

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\drivers\");
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://www.google.com";
+
+            // type | id=lst-ib | pluralsight
+            driver.FindElement(By.Id("lst-ib")).Clear();
+            driver.FindElement(By.Id("lst-ib")).SendKeys("pluralsight");
+            // click | id=_lsb | 
+            driver.FindElement(By.ClassName("lsb")).Click();
+            // click | link=Images | 
+            driver.FindElement(By.LinkText("Images")).Click();
+        }
+    }
+}
